9ffa30f3d8a339fb659a42f7ac372e3f9de77753
Nils Schönwald <nils@schoenwald.media>
2020-07-14 12:40:56 +0200

    update packages, change config

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-const production = process.env.NODE_ENV == 'production';
+const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   env: {
@@ -7,20 +7,48 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
   },
   extends: [
-    'eslint:recommended',
+    'airbnb-typescript',
     'plugin:import/errors',
     'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
   ],
-  plugins: ['filenames'],
+  plugins: [
+    'filenames',
+    'react',
+    'react-hooks',
+    'prettier',
+    'simple-import-sort',
+  ],
   rules: {
     'no-console': [production ? 2 : 1, { allow: ['error', 'warn', 'info'] }],
     'no-debugger': production ? 2 : 1,
     'filenames/match-exported': 2,
-    // "import/order": ["error", {"newlines-between": "always"}]
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+      },
+    ],
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-boolean-value': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/prop-types': 0,
+    'simple-import-sort/sort': 'error',
+    '@typescript-eslint/semi': 0,
   },
 };
