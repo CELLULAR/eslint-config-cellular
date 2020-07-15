80281c99b6af1a8eed6febd8cf5970a7c684439e
Nils Schönwald <nils@schoenwald.media>
2020-07-15 09:59:08 +0200

    edit prettier config

--- prettier.js
+++ prettier.js
@@ -1,15 +1,7 @@
-const { hasFile, hasPkgProp } = require('about-this-app');
-const builtIns = require('./prettier.config');
-
-const hasPrettierRc =
-  hasFile('.prettierrc') ||
-  hasFile('.prettier.config.js') ||
-  hasPkgProp('prettier');
-
 module.exports = {
   extends: ['prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error', hasPrettierRc ? null : builtIns],
+    'prettier/prettier': ['error'],
   },
 };
