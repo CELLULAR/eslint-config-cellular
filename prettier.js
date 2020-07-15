f27a9c75136059488926e81f35d7c3aa070d5cd1
Nils Schönwald <nils@schoenwald.media>
2020-07-15 10:52:03 +0200

    move config to sub-configs

--- prettier.js
+++ prettier.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['prettier'],
+  extends: [
+    'prettier',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
+  ],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error'],
--- react.js
+++ react.js
@@ -1,14 +1,13 @@
-const { hasDevDep } = require('about-this-app');
-
-const rules = {
-  'react/jsx-no-target-blank': 0,
-  'react/no-unescaped-entities': 0,
-};
-
-if (hasDevDep('flow-bin')) rules['react/prop-types'] = 0;
-
 module.exports = {
-  extends: ['plugin:react/recommended', 'prettier/react'],
-  plugins: ['react'],
-  rules,
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/jsx-no-target-blank': 0,
+    'react/no-unescaped-entities': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-boolean-value': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/prop-types': 0,
+  },
 };
