cad1d04c727fea3498e52f5fb44e8bbe424269cf
Nils Schönwald <nils@schoenwald.media>
2020-08-25 17:44:39 +0200

    add no-underscore-dangle

--- rulesCustom.js
+++ rulesCustom.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'no-console': [production ? 2 : 1, { allow: ['error', 'warn', 'info'] }],
     'no-debugger': production ? 2 : 1,
+    'no-underscore-dangle': 0,
     'filenames/match-exported': 2,
     'import/no-extraneous-dependencies': [
       'error',
