9e77a9cf687d541302ff3814bd6b2fa7540fc071
Nils Schönwald <nils@schoenwald.media>
2020-08-26 11:18:48 +0200

    remove filenames/match-exported

--- rulesCustom.js
+++ rulesCustom.js
@@ -2,14 +2,13 @@ const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   extends: ['plugin:import/errors', 'plugin:import/warnings'],
-  plugins: ['filenames', 'simple-import-sort'],
+  plugins: ['simple-import-sort'],
   rules: {
     'no-console': [production ? 2 : 1, { allow: ['error', 'warn', 'info'] }],
     'no-debugger': production ? 2 : 1,
     'no-underscore-dangle': 0,
     'import/prefer-default-export': 0,
     'simple-import-sort/sort': 'warn',
-    'filenames/match-exported': 2,
     'import/no-extraneous-dependencies': [
       'error',
       {
